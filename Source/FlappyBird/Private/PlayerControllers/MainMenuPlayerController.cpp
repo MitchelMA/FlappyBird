53073418319a165722243a51569f7c8530e45731
MitchelMA <32548@ma-web.nl>
2024-05-08 15:28:18 +0200

    begin widget for the main menu

--- Source/FlappyBird/Private/PlayerControllers/MainMenuPlayerController.cpp
+++ Source/FlappyBird/Private/PlayerControllers/MainMenuPlayerController.cpp
@@ -1,13 +1,31 @@
 
 #include "PlayerControllers/MainMenuPlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+DEFINE_LOG_CATEGORY(LogMainMenuPlayerController)
 
 AMainMenuPlayerController::AMainMenuPlayerController()
 {
+	const ConstructorHelpers::FClassFinder<UUserWidget> MainMenuWidgetClass(TEXT("/Game/FlappyBird/Blueprints/Widgets/WBP_MainMenu"));
+	MainMenuWidget = MainMenuWidgetClass.Class;
 }
 
 void AMainMenuPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	
+	if (!IsValid(MainMenuWidget))
+	{
+		UE_LOG(LogMainMenuPlayerController, Error,
+			TEXT("Unable to find User Widget of the Main Menu"));
+		return;
+	}
+
+	const auto CreatedWidget = CreateWidget(
+		GetWorld()->GetFirstPlayerController(),
+		MainMenuWidget,
+		TEXT("Main Menu")
+	);
+	CreatedWidget->AddToViewport();
+	SetShowMouseCursor(true);
 }
--- Source/FlappyBird/Public/PlayerControllers/MainMenuPlayerController.h
+++ Source/FlappyBird/Public/PlayerControllers/MainMenuPlayerController.h
@@ -4,12 +4,14 @@
 #include "GameFramework/PlayerController.h"
 #include "MainMenuPlayerController.generated.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogMainMenuPlayerController, Log, All)
+
 UCLASS()
 class FLAPPYBIRD_API AMainMenuPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-	
+	TSubclassOf<UUserWidget> MainMenuWidget;
 
 public:
 	AMainMenuPlayerController();
