8c00bf59134b4723f798c61659c450d1547cc222
MitchelMA <vonkm124@gmail.com>
2024-06-15 19:41:41 +0200

    displays the score/highscore to the game over widget

---
+++ Source/FlappyBird/Private/Interfaces/GameOverWidget.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/GameOverWidget.h"
+
+
+// Add default functionality here for any IGameOverWidget functions that are not pure virtual.
---
+++ Source/FlappyBird/Public/Interfaces/GameOverWidget.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GameOverWidget.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(BlueprintType)
+class UGameOverWidget : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class FLAPPYBIRD_API IGameOverWidget
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void SetWidgetDisplayScore(const int64 DisplayScore);
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void SetWidgetDisplayHighScore(const int64 DisplayHighScore);
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void SaveScore();
+};
